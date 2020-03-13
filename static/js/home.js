2031c624784dddf80ce336ad203b66ebb3c36497
cinnamon-rolling <jyng.9922@gmail.com>
2020-03-13 11:15:08 +0800

    [Feature] Add ajac for login bank account

--- static/js/home.js
+++ static/js/home.js
@@ -2,86 +2,127 @@ var cloneCount = 0;
 var hasOpened = false;
 var message;
 
-$(document).ready(function() {
-  
-  initialize();
+$(document).ready(function () {
 
-  if(performance.navigation.type == performance.navigation.TYPE_RELOAD){
-    console.info("Page reloaded!");
-    if (agentInfo){
-      $.ajax({
-        url: '/disconnect',
-        type: 'POST',
-        data: {agentID: agentInfo.id},
-        success: function(data, status, els){
-            console.log(`Agent [${data.id}] freed successfully!`);
-        },
-        error: function(err){
-            console.error(err.responseText);
+    initialize();
+
+    if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+        console.info("Page reloaded!");
+        if (agentInfo) {
+            $.ajax({
+                url: '/disconnect',
+                type: 'POST',
+                data: {
+                    agentID: agentInfo.id
+                },
+                success: function (data, status, els) {
+                    console.log(`Agent [${
+                        data.id
+                    }] freed successfully!`);
+                },
+                error: function (err) {
+                    console.error(err.responseText);
+                }
+            });
         }
-      });
     }
-  }
 
-  $(".chat").hide();
-
-  $(".chat-img").click(function() {
-    if (!hasOpened){
-      generateBotChoicesBubble();
-      hasOpened = true;
-    }
-    $(".chat").show();
-    $(".chat").animate({ opacity: "1.0", bottom: "20px" }, "slow");
-    $(".toggle-chat-btn").animate({ opacity: "0.0" }, "slow");
-    $(".toggle-chat-btn").hide();
-  });
-
-  $("form").submit(function(e) {
-    e.preventDefault();
-    // append chat message bubble
-    message = $("#userInputMsg").val();
-    generateSendBubble(message);
-    if (mConversation){
-      rainbowSDK.im.sendMessageToConversation(mConversation, message);
-    }
-    document.getElementById("userInputMsg").value = "";
-
-  });
-
-  $("#close").click(function() {
-    $(".chat").animate({ opacity: "0.0", bottom: "0" }, "slow");
-    $(".chat").hide();
-    $(".toggle-chat-btn").show();
-    $(".toggle-chat-btn").animate({ opacity: "1.0" }, "slow");
-  });
-  $("#quit").click(function() {
-    var decision = confirm(
-      "You are in Guest mode. Exit will erase all dialogue history."
-    );
-    if (decision) {
-      createAjax("GET", "/delete", {}, function(data, status, els) {
-        console.log(`${status}: ${data.id} has been deleted successfully!`);
-        window.location.href = "/";
-      });
-    } else {
-      return false;
-    }
-  });
-
-  $("#registerBtn").click(function() {
-    var username = $("#usernameInput").val();
-    $.ajax({
-      url: "/register",
-      data: {
-        username: username,
-        password: "whatever",
-        firstName: "David",
-        lastName: "Lee"
-      },
-      type: "POST",
-      success: function(data, status, r) {
-        console.log(status);
-      }
+    $("#submit_btn").click(function () {
+        var usernameInput = $("#usernameInput").val();
+        var passwordInput = $("#passwordInput").val();
+        if (username != '' && password != '') {
+            $.ajax({
+                url: '/login',
+                type: 'POST',
+                data: {
+                    username: usernameInput,
+                    password: passwordInput
+                },
+                success: function (data, status, r) {
+                    console.log(`First Name: ${
+                        data.firstName
+                    } Last Name: ${
+                        data.lastName
+                    }`);
+                },
+                error: function (err) {
+                    console.log('User not found');
+                }
+            })
+        } else {
+            return false;
+        }
     });
-  });
+
+    $(".chat").hide();
+
+    $(".chat-img").click(function () {
+        if (! hasOpened) {
+            generateBotChoicesBubble();
+            hasOpened = true;
+        }
+        $(".chat").show();
+        $(".chat").animate({
+            opacity: "1.0",
+            bottom: "20px"
+        }, "slow");
+        $(".toggle-chat-btn").animate({
+            opacity: "0.0"
+        }, "slow");
+        $(".toggle-chat-btn").hide();
+    });
+
+    $("form").submit(function (e) {
+        e.preventDefault();
+        // append chat message bubble
+        message = $("#userInputMsg").val();
+        generateSendBubble(message);
+        if (mConversation) {
+            rainbowSDK.im.sendMessageToConversation(mConversation, message);
+        }
+        document.getElementById("userInputMsg").value = "";
+
+    });
+
+    $("#close").click(function () {
+        $(".chat").animate({
+            opacity: "0.0",
+            bottom: "0"
+        }, "slow");
+        $(".chat").hide();
+        $(".toggle-chat-btn").show();
+        $(".toggle-chat-btn").animate({
+            opacity: "1.0"
+        }, "slow");
+    });
+    $("#quit").click(function () {
+        var decision = confirm("You are in Guest mode. Exit will erase all dialogue history.");
+        if (decision) {
+            createAjax("GET", "/delete", {}, function (data, status, els) {
+                console.log(`${status}: ${
+                    data.id
+                } has been deleted successfully!`);
+                window.location.href = "/";
+            });
+        } else {
+            return false;
+        }
+    });
+
+    $("#registerBtn").click(function () {
+        var username = $("#usernameInput").val();
+        $.ajax({
+            url: "/register",
+            data: {
+                username: username,
+                password: "whatever",
+                firstName: "David",
+                lastName: "Lee"
+            },
+            type: "POST",
+            success: function (data, status, r) {
+                console.log(status);
+            }
+        });
+    });
 });
