4e683bd292eefaa9b4989c48525c4aad2aea7408
BBFISH <44829092+xmliszt@users.noreply.github.com>
2020-04-04 20:03:48 +0800

    First Deployment!

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const app = require('./app');
+const PORT = process.env.PORT || 3000;
 
 process.env.NODE_ENV = "production";
 
 // app.httpServer.listen(8080);
-app.httpsServer.listen(8443);
+app.httpsServer.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);});
