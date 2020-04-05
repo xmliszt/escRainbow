116b33a15cb25199f6714126573c7d32d28fea92
BBFISH <44829092+xmliszt@users.noreply.github.com>
2020-04-05 22:19:51 +0800

    update dev env

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const app = require('./app');
 
 // PRODUCTION
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
-process.env.NODE_ENV = "production";
+// process.env.NODE_ENV = "production";
 
-// app.listen(8080);
-app.app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);});
+// // app.listen(8080);
+// app.app.listen(PORT, () => {
+//     console.log(`Our app is running on port ${ PORT }`);});
 
 
 // DEV
-// app.httpsServer.listen(8443);
+app.httpsServer.listen(8443);
