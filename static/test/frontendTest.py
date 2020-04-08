431f1e6b08643259f10fcc088832e52f83e3b409
cinnamon-rolling <jyng.9922@gmail.com>
2020-04-08 15:57:14 +0800

    [Add] Edit frontendTest

--- static/test/frontendTest.py
+++ static/test/frontendTest.py
@@ -9,117 +9,66 @@ class fronendTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    # enter username and password
+    def username_password(self, username_input, password_input):
+        driver = self.driver
+        driver.get("https://alpha-holding.herokuapp.com/")
+        time.sleep(3)
+
+        # get icon for chat
+        chat_icon = driver.find_element_by_class_name("toggle-chat-btn")
+        chat_icon.click()
+        time.sleep(3)
+        assert int(driver.find_elements_by_css_selector(
+            '.chat')[0].value_of_css_property('opacity')) == 1
+        time.sleep(3)
+
+        card_replacement = driver.find_element_by_xpath(
+            '//*[@id="1"]')
+        card_replacement.click()
+        time.sleep(3)
+
+        yes_please = driver.find_element_by_xpath(
+            '//*[@id="cd-0"]')
+        yes_please.click()
+        time.sleep(3)
+
+        sign_in = driver.find_element_by_xpath(
+            '//*[@id="resignin-5"]')
+        sign_in.click()
+        time.sleep(3)
+
+        # enter username and password
+        username = driver.find_element_by_id("usernameInput")
+        password = driver.find_element_by_id("passwordInput")
+        time.sleep(3)
+
+        username.send_keys(username_input)
+        password.send_keys(password_input)
+        time.sleep(3)
+
+        driver.find_element_by_id("login_btn").click()
+        time.sleep(3)
+
+        alert = driver.find_element_by_xpath('//*[@id="alertLogin"]')
+        time.sleep(3)
+
+        # register button, as register button will appear if user is not registered
+        if alert is not None:
+            pass
+        else:
+            assert driver.find_element_by_id('signInRegisterBtn') is not None
+            driver.find_element_by_id('signInRegisterBtn').click()
+            assert str(driver.find_elements_by_css_selector(
+                '.modal')[0].value_of_css_property('display')) == 'block'
 
     def test_username_password1(self):
-        driver = self.driver
-        driver.get("https://alpha-holding.herokuapp.com/")
+        self.username_password('test1', 'test1')
 
-        # get icon for chat
-        chat_icon = driver.find_element_by_class_name("toggle-chat-btn")
-        chat_icon.click()
-        time.sleep(3)
-
-        card_replacement = driver.find_element_by_xpath(
-            '//*[@id="1"]')
-        card_replacement.click()
-        time.sleep(3)
-
-        yes_please = driver.find_element_by_xpath(
-            '//*[@id="cd-0"]')
-        yes_please.click()
-        time.sleep(3)
-
-        sign_in = driver.find_element_by_xpath(
-            '//*[@id="resignin-5"]')
-        sign_in.click()
-        time.sleep(3)
-
-        # enter username and password
-        username = driver.find_element_by_id("usernameInput")
-        password = driver.find_element_by_id("passwordInput")
-        time.sleep(3)
-
-        username.send_keys("test1")
-        password.send_keys("test1")
-        time.sleep(3)
-
-        driver.find_element_by_id("login_btn").click()
-        time.sleep(3)
-
-    # enter username and password
     def test_username_password2(self):
-        driver = self.driver
-        driver.get("https://alpha-holding.herokuapp.com/")
+        self.username_password('', '')
 
-        # get icon for chat
-        chat_icon = driver.find_element_by_class_name("toggle-chat-btn")
-        chat_icon.click()
-        time.sleep(3)
-
-        card_replacement = driver.find_element_by_xpath(
-            '//*[@id="1"]')
-        card_replacement.click()
-        time.sleep(3)
-
-        yes_please = driver.find_element_by_xpath(
-            '//*[@id="cd-0"]')
-        yes_please.click()
-        time.sleep(3)
-
-        sign_in = driver.find_element_by_xpath(
-            '//*[@id="resignin-5"]')
-        sign_in.click()
-        time.sleep(3)
-
-        # enter username and password
-        username = driver.find_element_by_id("usernameInput")
-        password = driver.find_element_by_id("passwordInput")
-        time.sleep(3)
-
-        username.send_keys("")
-        password.send_keys("")
-        time.sleep(3)
-
-        driver.find_element_by_id("login_btn").click()
-        time.sleep(3)
-
-    # enter username and password
     def test_username_password3(self):
-        driver = self.driver
-        driver.get("https://alpha-holding.herokuapp.com/")
-
-        # get icon for chat
-        chat_icon = driver.find_element_by_class_name("toggle-chat-btn")
-        chat_icon.click()
-        time.sleep(3)
-
-        card_replacement = driver.find_element_by_xpath(
-            '//*[@id="1"]')
-        card_replacement.click()
-        time.sleep(3)
-
-        yes_please = driver.find_element_by_xpath(
-            '//*[@id="cd-0"]')
-        yes_please.click()
-        time.sleep(3)
-
-        sign_in = driver.find_element_by_xpath(
-            '//*[@id="resignin-5"]')
-        sign_in.click()
-        time.sleep(3)
-
-        # enter username and password
-        username = driver.find_element_by_id("usernameInput")
-        password = driver.find_element_by_id("passwordInput")
-        time.sleep(3)
-
-        username.send_keys("%ssomething")
-        password.send_keys("%d1334")
-        time.sleep(3)
-
-        driver.find_element_by_id("login_btn").click()
-        time.sleep(3)
+        self.username_password('&ssomething', '%d1234')
 
     def tearDown(self):
         self.driver.close()
