4f17cbc28fcf730d491b70d24982025ca9b93754
BBFISH <44829092+xmliszt@users.noreply.github.com>
2020-04-20 12:24:24 +0800

    dev config

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const fs = require("fs");
 const http = require("http");
 const https = require("https");
-const privateKey = fs.readFileSync('./sslcert/privateKey.key', 'utf8');
-const certificate = fs.readFileSync('./sslcert/certificate.crt', 'utf8');
-const appCredentials = {key: privateKey, cert: certificate};
+// const privateKey = fs.readFileSync('./sslcert/privateKey.key', 'utf8');
+// const certificate = fs.readFileSync('./sslcert/certificate.crt', 'utf8');
+// const appCredentials = {key: privateKey, cert: certificate};
 
 
 const shield = require("helmet");
@@ -385,5 +385,5 @@ app.get("/su/dashboard/delete", async(req, res) => {
 
 
 var httpServer = http.createServer(app);
-var httpsServer = https.createServer(appCredentials, app);
-module.exports = {app: app, httpsServer: httpsServer};
+// var httpsServer = https.createServer(appCredentials, app);
+module.exports = {app: app, httpServer: httpServer};
